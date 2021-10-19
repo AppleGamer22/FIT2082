a68b61b6b8865bd23e0f0b021e3b8dcaf78b4265
Omri Bornstein <omribor@gmail.com>
2021-10-19 13:31:29 +1100

    removed compile-only files

--- geas/fzn/check.mli
+++
@@ -1,6 +0,0 @@
-(* Check a model *)
-exception Not_satisfied of string
-
-val check : Problem.t -> Solver.intvar array -> Atom.t array -> Solver.model -> bool
-
-val check_exn : Problem.t -> Solver.intvar array -> Atom.t array -> Solver.model -> unit
